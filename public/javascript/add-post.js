0ebda71b3e5b2f429babc73bf43c727f183a9875
Diana Salas <salasd10418@gmail.com>
2021-09-27 23:30:44 -0400

    added modal for created post confirmation

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,10 +1,17 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("create-post");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 async function bookPostHandler(event) {
     event.preventDefault();
     const book_name = document.querySelector('input[name="book-name"]').value.trim();
     const book_author = document.querySelector('input[name="book-author"]').value.trim();
     const price = document.querySelector('input[name="price"]').value.trim();
-    const content = document.querySelector('textarea[name="content"]').value;
-console.log("success");
+    const content = document.querySelector('textarea[name="content"]').value.trim();
+
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
@@ -22,6 +29,23 @@ console.log("success");
     } else {
       alert(response.statusText);
     }
+}
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 document.querySelector('.book-post-form').addEventListener('submit', bookPostHandler);
